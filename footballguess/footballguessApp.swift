88cc3949dd14a03dafdddc59d977fdf43b74a1ee
anuarars <anuar_ars@hotmail.com>
2021-08-14 22:39:39 +0600

    Initial Commit

---
+++ footballguess/footballguessApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballguessApp.swift
+//  footballguess
+//
+//  Created by Anuar Arystanbekov on 14.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct footballguessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
